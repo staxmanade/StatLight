3dd1c16c279c2d3ce9721f59c49526f6f1aedea5
Jason Jarrett <jason@elegantcode.com>
2010-02-16 21:23:41 -0800

    Added the TestExecutionMethodPassedClientEvent event

---
+++ src/StatLight.Client.Harness/Server.cs
@@ -0,0 +1,17 @@
+﻿using StatLight.Client.Model.Events;
+using StatLight.Core.Serialization;
+
+namespace StatLight.Client.Harness
+{
+    public static class Server
+    {
+        public static void Trace(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+                message = "{StatLight - Trace Message: trace string is NULL or empty}";
+            var traceClientEvent = new TraceClientEvent { Message = message };
+            string traceMessage = traceClientEvent.Serialize();
+            StatLightPostbackManager.PostMessage(traceMessage);
+        }
+    }
+}
