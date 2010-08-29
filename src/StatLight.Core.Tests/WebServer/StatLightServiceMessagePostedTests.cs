4807dde9d5e9b5c4057cc8bf107a6c3cf9f0ba1f
Jason Jarrett <jason@elegantcode.com>
2010-08-28 18:01:17 -0700

    Removed unused file

--- src/StatLight.Core.Tests/WebServer/StatLightServiceMessagePostedTests.cs
+++
@@ -1,9 +0,0 @@
-using NUnit.Framework;
-namespace StatLight.Core.Tests.WebServer
-{
-    namespace StatLightServiceMessagePostedTests
-    {
-        //[TestFixture]
-        //public class when_a_
-    }
-}
