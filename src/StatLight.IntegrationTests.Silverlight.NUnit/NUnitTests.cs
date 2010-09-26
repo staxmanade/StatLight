c3b688fc13213ad303b21a1bb27da5e2e940394d
Jason Jarrett <jason@elegantcode.com>
2010-09-26 14:19:33 -0700

    tabs to spaces

--- src/StatLight.IntegrationTests.Silverlight.NUnit/NUnitTests.cs
+++ src/StatLight.IntegrationTests.Silverlight.NUnit/NUnitTests.cs
@@ -1,60 +1,60 @@
 ﻿
 namespace StatLight.IntegrationTests.Silverlight
 {
-	using NUnit.Framework;
+    using NUnit.Framework;
 
-	[TestFixture]
-	public class NUnitTests
-	{
-		[TestFixture]
-		public class NUnitNestedClassTests
-		{
-			[Test]
-			public void this_should_be_a_passing_test()
-			{
-				Assert.IsTrue(true);
-			}
-		}
+    [TestFixture]
+    public class NUnitTests
+    {
+        [TestFixture]
+        public class NUnitNestedClassTests
+        {
+            [Test]
+            public void this_should_be_a_passing_test()
+            {
+                Assert.IsTrue(true);
+            }
+        }
 
-		private object setupObject;
+        private object setupObject;
 
-		[SetUp]
-		public void Setup()
-		{
-			setupObject = new object();
-		}
+        [SetUp]
+        public void Setup()
+        {
+            setupObject = new object();
+        }
 
-		[Test]
-		public void this_should_be_a_passing_test()
-		{
-			Assert.IsTrue(true);
-		}
+        [Test]
+        public void this_should_be_a_passing_test()
+        {
+            Assert.IsTrue(true);
+        }
 
-		[Test]
-		public void this_should_also_be_a_passing_test()
-		{
-			Assert.IsTrue(true);
-		}
+        [Test]
+        public void this_should_also_be_a_passing_test()
+        {
+            Assert.IsTrue(true);
+        }
 
-		[Test]
-		public void this_should_be_a_Failing_test()
-		{
-			Assert.IsTrue(false);
-		}
+        [Test]
+        public void this_should_be_a_Failing_test()
+        {
+            Assert.IsTrue(false);
+        }
 
-		[Test]
-		[Ignore]
-		public void this_should_be_an_Ignored_test()
-		{
-			Assert.Fail("Should have been ignored");
-		}
+        [Test]
+        [Ignore]
+        public void this_should_be_an_Ignored_test()
+        {
+            Assert.Fail("Should have been ignored");
+        }
 
-		[Test]
-		[Category("xxx")]
-		public void the_setup_object_should_not_be_null()
-		{
-			Assert.IsNotNull(setupObject);
-		}
+        [Test]
+        [Category("xxx")]
+        public void the_setup_object_should_not_be_null()
+        {
+            Assert.IsNotNull(setupObject);
+        }
 
 
         [Test]
@@ -63,5 +63,5 @@ namespace StatLight.IntegrationTests.Silverlight
         {
             Assert.AreEqual("Hello", value);
         }
-	}
+    }
 }
