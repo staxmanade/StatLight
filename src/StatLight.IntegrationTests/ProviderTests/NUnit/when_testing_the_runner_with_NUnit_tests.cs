b89a0cf07ef4c9fda841c40357977947e75c4f0e
Jason Jarrett <jason@elegantcode.com>
2010-09-26 19:40:53 -0700

    Fixed up an integration test

--- src/StatLight.IntegrationTests/ProviderTests/NUnit/when_testing_the_runner_with_NUnit_tests.cs
+++ src/StatLight.IntegrationTests/ProviderTests/NUnit/when_testing_the_runner_with_NUnit_tests.cs
@@ -5,41 +5,40 @@ using StatLight.Core.UnitTestProviders;
 
 namespace StatLight.IntegrationTests.ProviderTests.NUnit
 {
-	[TestFixture]
-	public class when_testing_the_runner_with_NUnit_tests
-		: IntegrationFixtureBase
-	{
-		private ClientTestRunConfiguration clientTestRunConfiguration;
+    [TestFixture]
+    public class when_testing_the_runner_with_NUnit_tests
+        : IntegrationFixtureBase
+    {
+        private ClientTestRunConfiguration clientTestRunConfiguration;
 
-		protected override ClientTestRunConfiguration ClientTestRunConfiguration
-		{
-			get { return this.clientTestRunConfiguration; }
-		}
+        protected override ClientTestRunConfiguration ClientTestRunConfiguration
+        {
+            get { return this.clientTestRunConfiguration; }
+        }
 
-		protected override void Before_all_tests()
-		{
+        protected override void Before_all_tests()
+        {
             base.Before_all_tests();
             base.PathToIntegrationTestXap = TestXapFileLocations.NUnit;
             this.clientTestRunConfiguration = new IntegrationTestClientTestRunConfiguration(UnitTestProviderType.NUnit);
-		}
+        }
 
+        [Test]
+        public void Should_have_correct_TotalFailed_count()
+        {
+            TestReport.TotalFailed.ShouldEqual(1, "Failed count wrong");
+        }
 
-		[Test]
-		public void Should_have_correct_TotalFailed_count()
-		{
-			TestReport.TotalFailed.ShouldEqual(1, "Failed count wrong");
-		}
+        [Test]
+        public void Should_have_correct_TotalPassed_count()
+        {
+            TestReport.TotalPassed.ShouldEqual(11, "Passed count wrong");
+        }
 
-		[Test]
-		public void Should_have_correct_TotalPassed_count()
-		{
-			TestReport.TotalPassed.ShouldEqual(10, "Passed count wrong");
-		}
-
-		[Test]
-		public void Should_have_correct_TotalIgnored_count()
-		{
-			TestReport.TotalIgnored.ShouldEqual(1, "Ignored count wrong");
-		}
-	}
+        [Test]
+        public void Should_have_correct_TotalIgnored_count()
+        {
+            TestReport.TotalIgnored.ShouldEqual(1, "Ignored count wrong");
+        }
+    }
 }
