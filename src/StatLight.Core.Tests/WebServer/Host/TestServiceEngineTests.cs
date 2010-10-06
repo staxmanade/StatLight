baa081fef09d29d35e12fd5af08d088a9cc9bdd4
Jason Jarrett <jason@elegantcode.com>
2010-10-05 22:54:23 -0700

    initial add of the test service engine (copied from the msft test framework)

---
+++ src/StatLight.Core.Tests/WebServer/Host/TestServiceEngineTests.cs
@@ -0,0 +1,18 @@
+using System;
+using NUnit.Framework;
+using StatLight.Core.WebServer.Host;
+
+namespace StatLight.Core.Tests.WebServer.Host
+{
+    public class TestServiceEngineTests
+    {
+        //[Test]
+        //public void Should_be_able_to_start_and_stop_service()
+        //{
+        //    var testServiceEngine = new TestServiceEngine("localhost", 8888, TimeSpan.FromSeconds(30));
+        //    testServiceEngine.Start();
+        //    testServiceEngine.Stop();
+        //    //testServiceEngine
+        //}
+    }
+}
