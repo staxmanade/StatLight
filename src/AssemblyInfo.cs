f49d79bfe982a5b4b6e538ba99ec02fc816e2c70
Jason Jarrett <jason@elegantcode.com>
2010-11-25 10:11:46 -0800

    upped the assembly version to v1.3

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("StatLight")]
-[assembly: AssemblyCopyright("Copyright © 2009-2010")]
+[assembly: AssemblyCopyright("Copyright © 2009-2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,7 +34,7 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.*")]
+[assembly: AssemblyVersion("1.3.*")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
 [assembly: CLSCompliant(false)]
 //TODO: [assembly: AssemblyKeyFile("StatLight.snk")]
