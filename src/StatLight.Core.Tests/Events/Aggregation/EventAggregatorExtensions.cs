6e96f8b3e8f9aff3f08bb7cd69d9f6312a2b5638
Jason Jarrett <jason@elegantcode.com>
2010-11-25 23:32:49 -0800

    adding missing file

---
+++ src/StatLight.Core.Tests/Events/Aggregation/EventAggregatorExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StatLight.Core.Events.Aggregation
+{
+    public static class EventAggregatorExtensions
+    {
+        public static IEventAggregator AddListener<T>(this IEventAggregator eventAggregator, Action<T> listener)
+        {
+            var delegateListener = new DelegateListener<T>(listener);
+            eventAggregator.AddListener(delegateListener);
+            return eventAggregator;
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1004:GenericMethodsShouldProvideTypeParameter")]
+        public static IEventAggregator AddListener<T>(this IEventAggregator eventAggregator, Action listener)
+        {
+            var delegateListener = new DelegateListener<T>(msg => listener());
+            eventAggregator.AddListener(delegateListener);
+            return eventAggregator;
+        }
+
+        public static IEventAggregator AddListener(this IEventAggregator eventAggregator, Action listener, Predicate<object> filter)
+        {
+            return eventAggregator.AddListener(listener, filter);
+        }
+
+        private class DelegateListener<T> : IListener<T>
+        {
+            private readonly Action<T> _listener;
+
+            public DelegateListener(Action<T> listener)
+            {
+                _listener = listener;
+            }
+
+            public void Handle(T message)
+            {
+                _listener(message);
+            }
+        }
+    }
+}
