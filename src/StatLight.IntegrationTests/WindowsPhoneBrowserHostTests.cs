6b4f026eaa6f30cf7ad6c7efcd74a411bd59f516
Jason Jarrett <jason@elegantcode.com>
2011-03-05 20:26:11 -0800

    initial test of starting/stopping the phone emulator

--- src/StatLight.IntegrationTests/WindowsPhoneBrowserHostTests.cs
+++ src/StatLight.IntegrationTests/WindowsPhoneBrowserHostTests.cs
@@ -1,22 +1,27 @@
 using NUnit.Framework;
 using StatLight.Core.Common;
 using StatLight.Core.Phone;
-using StatLight.IntegrationTests.ProviderTests;
 
 namespace StatLight.IntegrationTests
 {
     public class WindowsPhoneBrowserHostTests : FixtureBase
     {
         [Test]
-        public void Should_NAME()
+        public void Should_work()
         {
             ILogger logger = new ConsoleLogger();
 
-            var testXapPath = TestXapFileLocations.WinPhone;
+            //var testXapPath = @"C:\Code\StatLight\src\StatLight.IntegrationTests.Phone.MSTest\Bin\Debug\StatLight.IntegrationTests.Phone.MSTest.xap";
+            var testXapPath = @"C:\Code\StatLight\src\StatLight.Client.Harness.Phone\Bin\Debug\StatLight.Client.Harness.Phone.xap";
+            //var testXapPath = TestXapFileLocations.WinPhone;
 
             var windowsPhoneBrowserHost = new WindowsPhoneBrowserHost(logger, testXapPath);
 
             windowsPhoneBrowserHost.Start();
+
+            System.Threading.Thread.Sleep(2000);
+
+            windowsPhoneBrowserHost.Stop();
         }
     }
 }
