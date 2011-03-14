34a9ea7f27af890762868a14ff8232b0edc9e09c
Jason Jarrett <jason@elegantcode.com>
2011-03-14 06:11:19 -0700

    moved 2 files into 1 file that always seem to be updated at the same time

--- src/StatLight.Core/WebServer/XapHost/XapHostType.cs
+++ src/StatLight.Core/WebServer/XapHost/XapHostType.cs
@@ -14,4 +14,25 @@
 
         UnitDrivenDecember2009,
     }
+
+    public enum MicrosoftTestingFrameworkVersion
+    {
+        //Obsolete versions
+        December2008 = 1,
+        March2009 = 2,
+
+        //Supported versions
+        July2009 = 3,
+        October2009 = 4,
+        November2009 = 5,
+
+        March2010 = 6,
+        April2010 = 7,
+
+        May2010 = 8,
+
+        Feb2011 = 9,
+
+        //FYI: go update StatLightClientXapNames.Default value when changing the default runner...
+    }
 }
