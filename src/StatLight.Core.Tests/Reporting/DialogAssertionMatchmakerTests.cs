fe358f373fa3a0bc9ada3ffb3ec86c34f03bcb93
Jason Jarrett <jason@elegantcode.com>
2011-12-03 16:35:22 -0800

    Fixed an issue where an out of order sequence of events with the begin test method and a dialog assertion could cause an error.

---
+++ src/StatLight.Core.Tests/Reporting/DialogAssertionMatchmakerTests.cs
@@ -0,0 +1,79 @@
+﻿using NUnit.Framework;
+using StatLight.Client.Harness.Events;
+using StatLight.Core.Events;
+using StatLight.Core.Reporting;
+
+namespace StatLight.Core.Tests.Reporting
+{
+    public class DialogAssertionMatchmakerTests
+    {
+        [Test]
+        public void Should_detect_assertion()
+        {
+            var testExecutionMethodBeginClientEvent = new TestExecutionMethodBeginClientEvent
+                                                          {
+                                                              NamespaceName = "a",
+                                                              ClassName = "b",
+                                                              MethodName = "c",
+                                                          };
+            var dialogAssertionServerEvent = new DialogAssertionServerEvent(DialogType.Assert)
+                                                 {
+                                                     Message = "at b.c",
+                                                 };
+            var dialogAssertionMatchmaker = new DialogAssertionMatchmaker();
+            var testExecutionMethod = new TestExecutionMethodPassedClientEvent
+                                          {
+                                              NamespaceName = "a",
+                                              ClassName = "b",
+                                              MethodName = "c",
+                                          };
+            bool match = false;
+
+
+            dialogAssertionMatchmaker.HandleMethodBeginClientEvent(testExecutionMethodBeginClientEvent);
+            dialogAssertionMatchmaker.AddAssertionHandler(dialogAssertionServerEvent, item =>
+            {
+                match = true;
+            });
+
+
+            dialogAssertionMatchmaker.WasEventAlreadyClosed(testExecutionMethod).ShouldBeTrue();
+            match.ShouldBeTrue();
+        }
+
+
+        [Test]
+        public void If_assettion_happens_before_begin_test_event_arrives_should_match()
+        {
+            var testExecutionMethodBeginClientEvent = new TestExecutionMethodBeginClientEvent
+            {
+                NamespaceName = "a",
+                ClassName = "b",
+                MethodName = "c",
+            };
+            var dialogAssertionServerEvent = new DialogAssertionServerEvent(DialogType.Assert)
+            {
+                Message = "at b.c",
+            };
+            var dialogAssertionMatchmaker = new DialogAssertionMatchmaker();
+            var testExecutionMethod = new TestExecutionMethodPassedClientEvent
+                                          {
+                                              NamespaceName = "a",
+                                              ClassName = "b",
+                                              MethodName = "c",
+                                          };
+            bool match = false;
+
+
+            dialogAssertionMatchmaker.AddAssertionHandler(dialogAssertionServerEvent, item =>
+            {
+                match = true;
+            });
+            dialogAssertionMatchmaker.HandleMethodBeginClientEvent(testExecutionMethodBeginClientEvent);
+
+
+            dialogAssertionMatchmaker.WasEventAlreadyClosed(testExecutionMethod).ShouldBeTrue();
+            match.ShouldBeTrue();
+        }
+    }
+}
--- src/StatLight.Core/Reporting/DialogAssertionMatchMaker.cs
+++ src/StatLight.Core/Reporting/DialogAssertionMatchMaker.cs
@@ -9,23 +9,37 @@ namespace StatLight.Core.Reporting
     public class DialogAssertionMatchmaker
     {
         private readonly List<TestExecutionMethodBeginClientEvent> _completedMessage = new List<TestExecutionMethodBeginClientEvent>();
-
-        private readonly Dictionary<DialogAssertionServerEvent, Action<TestExecutionMethodBeginClientEvent>> _dialogAssertionEventsWithHandlers =
-            new Dictionary<DialogAssertionServerEvent, Action<TestExecutionMethodBeginClientEvent>>();
+        private readonly Dictionary<DialogAssertionServerEvent, Action<TestExecutionMethodBeginClientEvent>> _dialogAssertionEventsWithHandlers = new Dictionary<DialogAssertionServerEvent, Action<TestExecutionMethodBeginClientEvent>>();
+        private readonly List<TestExecutionMethodBeginClientEvent> _notHandledYet = new List<TestExecutionMethodBeginClientEvent>();
 
         public void HandleMethodBeginClientEvent(TestExecutionMethodBeginClientEvent message)
         {
-            if (_dialogAssertionEventsWithHandlers.Any(w => w.Key.Message.Contains(message.MethodName)))
+            var searchFor = GetItemToSearchFor(message);
+
+            if (_dialogAssertionEventsWithHandlers.Any(w => w.Key.Message.Contains(searchFor)))
             {
-                var x = _dialogAssertionEventsWithHandlers.First(w => w.Key.Message.Contains(message.MethodName));
-                _completedMessage.Add(message);
-                x.Value(message);
+                var x = _dialogAssertionEventsWithHandlers.First(w => w.Key.Message.Contains(searchFor));
+                MarkItMatched(message, x.Value);
+            }
+            else
+            {
+                _notHandledYet.Add(message);
             }
         }
 
         public void AddAssertionHandler(DialogAssertionServerEvent message, Action<TestExecutionMethodBeginClientEvent> onMatched)
         {
-            _dialogAssertionEventsWithHandlers.Add(message, onMatched);
+            var item = _notHandledYet.FirstOrDefault(begin => message.Message.Contains(GetItemToSearchFor(begin)));
+
+            if(item ==  null)
+            {
+                _dialogAssertionEventsWithHandlers.Add(message, onMatched);
+            }
+            else
+            {
+                _notHandledYet.Remove(item);
+                MarkItMatched(item, onMatched);
+            }
         }
 
         public bool WasEventAlreadyClosed(TestExecutionMethod message)
@@ -34,6 +48,17 @@ namespace StatLight.Core.Reporting
                                          a.NamespaceName == message.NamespaceName
                                          && a.ClassName == message.ClassName
                                          && a.MethodName == message.MethodName);
+        }
+
+        private static string GetItemToSearchFor(TestExecutionMethodBeginClientEvent message)
+        {
+            return message.ClassName + "." + message.MethodName;
+        }
+
+        private void MarkItMatched(TestExecutionMethodBeginClientEvent message, Action<TestExecutionMethodBeginClientEvent> action)
+        {
+            _completedMessage.Add(message);
+            action(message);
         }
     }
 }
