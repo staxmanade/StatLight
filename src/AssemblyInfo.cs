516f21bbc72aae8405ef8c9fca1558cb9819b1c7
Jason Jarrett <jason@elegantcode.com>
2011-12-20 20:20:59 -0800

    Upping the version from 1.5 to 1.6 in prep for a release

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.*")]
+[assembly: AssemblyVersion("1.6.*")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
 [assembly: CLSCompliant(false)]
 //TODO: [assembly: AssemblyKeyFile("StatLight.snk")]
