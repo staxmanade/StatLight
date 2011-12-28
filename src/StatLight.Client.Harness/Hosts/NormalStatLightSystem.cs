4765ae7958d21662a83c44ad09317a135fe19cea
Jason Jarrett <jason@elegantcode.com>
2011-12-28 11:35:54 -0800

    factored out settings into it's own class

--- src/StatLight.Client.Harness/Hosts/NormalStatLightSystem.cs
+++ src/StatLight.Client.Harness/Hosts/NormalStatLightSystem.cs
@@ -11,23 +11,12 @@ namespace StatLight.Client.Harness.Hosts
     {
         internal NormalStatLightSystem(Action<UIElement> onReady)
         {
-            var appManifestXml = XDocument.Load("StatLight.Settings.xml");
-            var portElement = appManifestXml.Root.Elements("Port").First();
-            var port = int.Parse(portElement.Value);
-            Setup(onReady, "http://localhost:{0}/".FormatWith(port));
-        }
-
-        private void Setup(Action<UIElement> onReady, string urlx)
-        {
-            SetPostbackUri(new Uri(urlx));
-
-            OnReadySetupRootVisual(onReady);
-        }
-
-        private void OnReadySetupRootVisual(Action<UIElement> onReady)
-        {
             if (onReady == null)
                 throw new ArgumentNullException("onReady");
+
+            string url = "http://localhost:{0}/".FormatWith(Settings.Port);
+
+            SetPostbackUri(new Uri(url));
 
             OnReady = onReady;
 
@@ -45,5 +34,18 @@ namespace StatLight.Client.Harness.Hosts
             DisplayTestHarness();
         }
     }
-}
 
+    public class Settings
+    {
+        static Settings()
+        {
+            var appManifestXml = XDocument.Load("StatLight.Settings.xml").Root;
+            Func<string, XElement> getElement = settingName => appManifestXml.Elements(settingName).First();
+            Func<string, string> getValue = settingName => getElement(settingName).Value;
+
+            Port = int.Parse(getValue("Port"));
+        }
+
+        public static int Port { get; private set; }
+    }
+}
